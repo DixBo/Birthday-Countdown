9467ff552e1f7073c2d4ee6b67a6e6182e8a1989
Dixon <budboden@yahoo.com>
2018-08-08 11:47:07 -0600

    styled clock with Css Grid and detailed styles

--- src/components/clock.js
+++ src/components/clock.js
@@ -4,21 +4,21 @@ class Clock extends Component{
     render() {
         return (
             <div className="clock">
-                <div className="clock__days">
-                    <label className="clock__title">days</label>
-                    <label className="clock__amount">20</label>
+                <div className="clock__days clock__box">
+                    <label className="clock__title">DAYS</label>
+                    <label className="clock__amount">300</label>
                 </div>
-                <div className="clock__hours">
-                    <label className="clock__title">hours</label>
-                    <label className="clock__amount">20</label>
+                <div className="clock__hours clock__box">
+                    <label className="clock__title">HRS</label>
+                    <label className="clock__amount">16</label>
                 </div>
-                <div className="clock__minutes">
-                    <label className="clock__title">minutes</label>
-                    <label className="clock__amount">20</label>
+                <div className="clock__minutes clock__box">
+                    <label className="clock__title">MINS</label>
+                    <label className="clock__amount">42</label>
                 </div>
-                <div className="clock__seconds">
-                    <label className="clock__title">seconds</label>
-                    <label className="clock__amount">20</label>
+                <div className="clock__seconds clock__box">
+                    <label className="clock__title">SECS</label>
+                    <label className="clock__amount">22</label>
                 </div>
             </div>
         )
