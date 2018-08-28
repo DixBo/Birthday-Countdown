1214d0614bc2da8d2c999b8b4ba05ce5dc180ca6
DixBo <budboden@yahoo.com>
2018-08-27 20:43:18 -0600

    bug fixes

--- src/components/app.js
+++ src/components/app.js
@@ -5,15 +5,54 @@ import Clock from './clock';
 import ChangeDate from './changeDate';
 import LargeText from './largeText';
 
+import moment from 'moment';
+
 export default class App extends Component {
 
   constructor(props) {
     super(props)
 
     this.state = {
-      active: false
+      active: false,
+      startDate: moment()
     }
   }
+
+  handleChange = function(date) {
+    console.log('APP JS HANDLE CHANGE', date._d)
+    this.setState({
+        startDate: date
+    });
+}.bind(this)
+
+handleGenerate = function() {
+  this.setState({ active: true })
+  // Set the date we're counting down to
+var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+ // Update the count down every 1 second
+var x = setInterval(function() {
+   // Get todays date and time
+  var now = new Date().getTime();
+  
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+  
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  // Output the result in an element with id="demo"
+  const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  console.log(time)
+   // If the count down is over, write some text 
+  if (distance < 0) {
+      clearInterval(x);
+      // document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+}.bind(this)
   
   renderItems = function() {
     if(this.state.active) {
@@ -25,7 +64,7 @@ export default class App extends Component {
       ]
     } else {
         return [
-          <Picker/>,
+          <Picker callback={(date) => this.handleChange(date)}/>,
           Button('Generate CountDown', () => this.setState({ active: true }))
         ]
     }
