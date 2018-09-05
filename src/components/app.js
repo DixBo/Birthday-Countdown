6ec35a7ec218060ac0bf53b95439fadd60fd65cf
DixBo <budboden@yahoo.com>
2018-09-05 09:27:41 -0600

    cleaned up picker.js

--- src/components/app.js
+++ src/components/app.js
@@ -12,10 +12,20 @@ export default class App extends Component {
   constructor(props) {
     super(props)
 
+    var timer = 0
+
     this.state = {
       active: false,
-      startDate: moment()
+      startDate: moment(),
+      timeRemaining: {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+      }
     }
+
+    this.handle
   }
 
   handleChange = function(date) {
@@ -30,7 +40,7 @@ handleGenerate = function() {
   // Set the date we're counting down to
 var countDownDate = this.state.startDate.toDate().getTime();
  // Update the count down every 1 second
-var x = setInterval(function() {
+timer = setInterval(function() {
    // Get todays date and time
   var now = new Date().getTime();
   
@@ -45,7 +55,15 @@ var x = setInterval(function() {
   
   // Output the result in an element with id="demo"
   const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-  console.log(time)
+    const timeRemaining = {
+        days,
+        hours,
+        minutes,
+        seconds
+    }
+    this.setState({ active: true})
+
+  console.log(this.state.timeRemaining)
    // If the count down is over, write some text 
   if (distance < 0) {
       clearInterval(x);
@@ -64,7 +82,7 @@ var x = setInterval(function() {
       ]
     } else {
         return [
-          <Picker callback={(date) => this.handleChange(date)}/>,
+          <Picker startDate={this.state.startDate} callback={(date) => this.handleChange(date)}/>,
           Button('Generate CountDown', () => this.handleGenerate())
         ]
     }
